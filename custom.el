66b290e7979753d348173f316a1b0c2a08eff680
laooree <andrea.lauri96@gmail.com>
2025-06-20 19:32:56 +0200

    Auctex use Evince.

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(TeX-view-program-selection
    '(((output-dvi has-no-display-manager) "dvi2tty")
      ((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi")
-     (output-pdf "Zathura") (output-html "xdg-open")))
+     (output-pdf "Evince") (output-html "xdg-open")))
  '(custom-safe-themes
    '("e8183add41107592ee785f9f9b4b08d21bd6c43206b85bded889cea1ee231337"
      "5ec088e25ddfcfe37b6ae7712c9cb37fd283ea5df7ac609d007cafa27dab6c64"
@@ -14,7 +14,7 @@
      "c0350f7d444071ac22bc1a430ac628a3578217cfe3fb4c0d7e1fd68cac4b4ed9"
      "36d8dbd35cfdd184e869efbeae66f3696c264e9d6eb7aa5952145dfde67146a7"
      "13517109e13a24f5d781b7a688c063dd91ae7808bb5be4486183cffe87a33b08" default))
- '(preview-TeX-style-dir "/home/laooree/.emacs.d/straight/build/auctex/latex"))
+ '(preview-TeX-style-dir "/home/laooree/.emacs.d/elpaca/builds/auctex/latex"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
