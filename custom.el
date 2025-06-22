f0e15412344aa5723bfdd62544e5dccfdb3e7e18
laooree <andrea.lauri96@gmail.com>
2025-06-22 10:47:54 +0200

    Let stylix manage the theme.

--- custom.el
+++ custom.el
@@ -8,7 +8,8 @@
      ((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi")
      (output-pdf "Evince") (output-html "xdg-open")))
  '(custom-safe-themes
-   '("e8183add41107592ee785f9f9b4b08d21bd6c43206b85bded889cea1ee231337"
+   '("27dac7a05a4dabd15ee4fec7c881b172cb8464a11afcf3de6ffad3c61f20247a"
+     "e8183add41107592ee785f9f9b4b08d21bd6c43206b85bded889cea1ee231337"
      "5ec088e25ddfcfe37b6ae7712c9cb37fd283ea5df7ac609d007cafa27dab6c64"
      "f9147c96259808e06969da1685889831ca8ae29e8104cae450825c6572872a84"
      "c0350f7d444071ac22bc1a430ac628a3578217cfe3fb4c0d7e1fd68cac4b4ed9"
