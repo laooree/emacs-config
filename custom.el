48b4b9f182ccd12b64eca3840b146f941a36f14f
laooree <andrea.lauri96@gmail.com>
2025-09-22 09:12:31 +0200

    package.el with native compilation.

--- custom.el
+++ custom.el
@@ -1,18 +1,22 @@
+;;; -*- lexical-binding: t -*-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-view-program-selection
-   '(((output-dvi has-no-display-manager) "dvi2tty")
-     ((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi")
-     (output-pdf "Evince") (output-html "xdg-open")))
- '(custom-safe-themes
-   '("27dac7a05a4dabd15ee4fec7c881b172cb8464a11afcf3de6ffad3c61f20247a"
-     "e8183add41107592ee785f9f9b4b08d21bd6c43206b85bded889cea1ee231337"
-     "5ec088e25ddfcfe37b6ae7712c9cb37fd283ea5df7ac609d007cafa27dab6c64"
-     "f9147c96259808e06969da1685889831ca8ae29e8104cae450825c6572872a84"
-     "c0350f7d444071ac22bc1a430ac628a3578217cfe3fb4c0d7e1fd68cac4b4ed9"
-     "36d8dbd35cfdd184e869efbeae66f3696c264e9d6eb7aa5952145dfde67146a7"
-     "13517109e13a24f5d781b7a688c063dd91ae7808bb5be4486183cffe87a33b08" default))
- '(preview-TeX-style-dir "/home/laooree/.emacs.d/elpaca/builds/auctex/latex"))
+ '(package-selected-packages
+   '(auctex beacon consult corfu-terminal denote dired-subtree doom-modeline
+            ef-themes evil-collection evil-numbers evil-snipe flymake-grammarly
+            format-all general haskell-mode highlight-indentation magit
+            marginalia matlab-mode modus-themes nerd-icons-completion
+            nerd-icons-corfu nerd-icons-dired nix-mode nyan-mode openwith
+            orderless org-auto-tangle org-modern rainbow-csv rainbow-delimiters
+            rainbow-mode spacious-padding vertico yasnippet-snippets))
+ '(package-vc-selected-packages
+   '((rainbow-csv :url "https://github.com/emacs-vs/rainbow-csv"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
