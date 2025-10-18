e6ec6f49310250f1a154ee74a3b8742594b4ba08
laooree <andrea.lauri96@gmail.com>
2025-10-18 18:06:10 +0200

    Minimalogy theme.

--- custom.el
+++ custom.el
@@ -4,16 +4,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(auctex beacon consult corfu-terminal denote dired-subtree doom-modeline
-            ef-themes evil-collection evil-numbers evil-snipe flymake-grammarly
-            format-all general haskell-mode highlight-indentation magit
-            marginalia matlab-mode modus-themes nerd-icons-completion
-            nerd-icons-corfu nerd-icons-dired nix-mode nyan-mode openwith
-            orderless org-auto-tangle org-modern rainbow-csv rainbow-delimiters
-            rainbow-mode spacious-padding vertico yasnippet-snippets))
+ '(package-selected-packages nil)
  '(package-vc-selected-packages
-   '((rainbow-csv :url "https://github.com/emacs-vs/rainbow-csv"))))
+   '((minimalogy-theme :url "https://github.com/laooree/minimalogy-theme.el"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
